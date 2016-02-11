e7de8711e194f575f27717f726ecc27f088fd3f6
Christopher Reynolds <reynolds87chris@gmail.com>
2016-02-11 10:10:50 -0700

    - Update sticky note button to show full shadow (image was cut off) - Add show/hide screen elements and math logic

--- Multiples Exercise/ViewController.swift
+++ Multiples Exercise/ViewController.swift
@@ -9,17 +9,60 @@
 import UIKit
 
 class ViewController: UIViewController {
+    // Properties
+    var maxTotal: Int = 100;
+    var multiple: Int = 0;
+    var total: Int = 0;
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+    // Outlets
+    @IBOutlet weak var logoImg: UIImageView!
+    @IBOutlet weak var whatMultipleTxt: UITextField!
+    @IBOutlet weak var letsMathBtn: UIButton!
+    
+    @IBOutlet weak var mathLbl: UILabel!
+    @IBOutlet weak var addBtn: UIButton!
+    
+    @IBAction func onLetsMathPressed(sender: UIButton) {
+        if whatMultipleTxt.text != nil && whatMultipleTxt.text != "" {
+            showHideStartScreen()
+            showHideMathScreen()
+            multiple = Int( whatMultipleTxt.text! )!
+            mathLbl.text = "Pres ADD to Add!"
+        }
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    
+    @IBAction func onAddPressed(sender: UIButton) {
+        mathLbl.text = "\(total) + \(multiple) = \(total + multiple)"
+        total += multiple
+        if total >= maxTotal {
+            showHideMathScreen()
+            showHideStartScreen()
+            total = 0
+            whatMultipleTxt.text = ""
+        }
     }
-
+    
+    func showHideStartScreen() {
+        if !logoImg.hidden {
+            logoImg.hidden = true
+            whatMultipleTxt.hidden = true
+            letsMathBtn.hidden = true
+        } else {
+            logoImg.hidden = false
+            whatMultipleTxt.hidden = false
+            letsMathBtn.hidden = false
+        }
+    }
+    
+    func showHideMathScreen() {
+        if !mathLbl.hidden {
+            mathLbl.hidden = true
+            addBtn.hidden = true
+        } else {
+            mathLbl.hidden = false
+            addBtn.hidden = false
+        }
+    }
 
 }
 
